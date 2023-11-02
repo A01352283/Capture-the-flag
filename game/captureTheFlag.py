1b124c581398bee460dc92d1db235ff8001fd4ab
Andres Briseno <A01352283@tec.mx>
2023-11-02 00:37:35 -0600

    Quality of life for CTF
    - Adds player score text
    - Makes it so that the player is considered to be on the other side when more that half of its body is on the other side

--- game/captureTheFlag.py
+++ game/captureTheFlag.py
@@ -4,6 +4,7 @@ import sys
 # Initialize Pygame
 pygame.init()
 clock = pygame.time.Clock()
+font = pygame.font.Font(None, 36)  # Choose the font for the score, None means default font
 
 # Set the frames per second (FPS)
 FPS = 60
@@ -60,7 +61,7 @@ while True:
     if keys[pygame.K_RIGHT] and player2.x + PLAYER_SPEED < WIDTH - PLAYER_SIZE:
         player2.x += PLAYER_SPEED
 
-    # Scoring
+   # Scoring
     if player1.colliderect(flag2):
         score1 += 1
         player1.x = 0
@@ -75,10 +76,10 @@ while True:
         player2.y = HEIGHT // 2
 
     # Player reset
-    if player1.colliderect(player2) and player1.x > HALF_WIDTH:
+    if player1.colliderect(player2) and player1.x + PLAYER_SIZE / 2 > HALF_WIDTH:
         player1.x = 0
         player1.y = HEIGHT // 2
-    if player2.colliderect(player1) and player2.x < HALF_WIDTH:
+    if player2.colliderect(player1) and player2.x + PLAYER_SIZE / 2 < HALF_WIDTH:
         player2.x = WIDTH - PLAYER_SIZE
         player2.y = HEIGHT // 2
 
@@ -88,6 +89,10 @@ while True:
     pygame.draw.rect(screen, BLUE, player2)
     pygame.draw.rect(screen, RED, flag1)
     pygame.draw.rect(screen, BLUE, flag2)
+
+    # Draw the scores
+    score_text = font.render(f"Player 1: {score1} Player 2: {score2}", True, WHITE)
+    screen.blit(score_text, (20, 20))  # Adjust the position as needed
 
     # Draw the dividing line
     pygame.draw.line(screen, WHITE, (HALF_WIDTH, 0), (HALF_WIDTH, HEIGHT), 2)
